2c6af5bbac63efe0202cb44fb21bd62cbf8d95b6
artcheng <artcheng@gmail.com>
2018-09-06 10:46:08 -0400

    test2

--- controllers/cambrController.js
+++ controllers/cambrController.js
@@ -2,6 +2,7 @@ var Message = require('../models/message');
 var db = require('./db');
 
 exports.programs_get = function(req, res) {
+	console.log("get cambr programs");
 	db.get('CAMBR').query('SELECT * FROM account_types', function(err, rows){
 		if(err){
 			 res.send("Error: cambr.programs.get " + err);
